9dc16079a52f7271b1a5022c5c9c110fd6838287
amanamazing100 <53084393+amanamazing100@users.noreply.github.com>
2020-05-14 13:32:03 +0530

    Update js_tut1.js

--- script/js_tut1.js
+++ script/js_tut1.js
@@ -1,275 +1,67 @@
-var canvas=document.querySelector('canvas');
-let c=canvas.getContext('2d');
-let cObs=canvas.getContext('2d');
-canvas.width=window.innerWidth*0.75;
-canvas.height=window.innerHeight;
-var paused=false;
-var score=0;
-var mySound=new Audio();
-mySound.src="bounce.wav";
-function togglePause(){
-	if(paused)
-	{
-		paused=false;
-		console.log(paused);
-	
-	}
-	else
-	{
-		paused=true;
-		console.log(paused);
-	}
-}
+function makeCanvas(){
+	var c1 = document.getElementById("canvas1");
+var ctx = c1.getContext("2d");
+var grd=ctx.createLinearGradient(50,20,250,100);
+grd.addColorStop(0,"Violet");
+grd.addColorStop(0.20,"Blue");
+grd.addColorStop(0.40,"Green");
+grd.addColorStop(0.65,"Yellow");
+grd.addColorStop(0.80,"Pink");
+grd.addColorStop(1,"Red");
+ctx.font = "90px Arial ";
+ctx.lineWidth=3;
+ctx.shadowBlur=10;
+ctx.shadowColor="Blue";
+ctx.fillStyle="Green";
+ctx.strokeStyle=grd;
+ctx.strokeText("dkjkm", 50,100);
+ 
+var c2 = document.getElementById('canvas2');
+var ctx2 = c2.getContext('2d');
 
-window.addEventListener('keydown', function(e){
-	let key=e.keyCode;
-	if(key==32)
-		togglePause();
-});
-		
-let colorArray=["red", "green", "pink", "yellow"];
-//circle
-var y=canvas.height*0.85;
-var acc=0.3;
-var velocity=0;
-var myBallColor=colorArray[Math.floor(Math.random()*4)];
-window.addEventListener('keydown', function(e){
-	let key=e.keyCode;
-	if(key==13)
-		start();
-});
+var grd2=ctx2.createRadialGradient(100,45,10,100,45,50);
+grd2.addColorStop(0,"Violet");
+grd2.addColorStop(0.20,"Blue");
+grd2.addColorStop(0.40,"Green");
+grd2.addColorStop(0.65,"Yellow");
+grd2.addColorStop(0.80,"Pink");
+grd2.addColorStop(1,"Red");
+ctx2.strokeStyle = "#11A1C6";
+ctx2.fillStyle = grd2;
+ctx2.shadowBlur=20;
+ctx2.shadowColor="Black";
+ctx2.shadowOffsetY=30;
+//ctx2.lineWidth = 10;
+ctx2.fillRect(50,10,100,75);
+ctx2.strokeRect(50,10,100,75);
 
-function displayLeaderboards(){
-	
-	document.getElementById('leaderboardsDisplay').textContent='Leaderboards';
-	let leaderText="highscore";
-	let ab=localStorage.getItem(leaderText);
-	let useArray=JSON.parse(ab);
-	let list1=document.createElement("ul");
-	list1.style.listStyleType="none";
-	console.log(useArray);
-	if(1){
-	var li1=document.createElement("li");
-	li1.appendChild(document.createTextNode(useArray));
-	list1.appendChild(li1);}
-	document.getElementById('leaderboardsDisplay').appendChild(list1);
-	};
+var c3=document.getElementById('canvas3');
+var ctx3 = c3.getContext('2d');
+ctx3.fllStyle = "Black";
+ctx3.strokeStyle = "Black";
+//ctx3.lineWidth = 5;
+ctx3.beginPath();
+ctx3.moveTo(50,85);
+ctx3.lineTo(95,10);
+ctx3.lineTo(140,85);
+ctx3.moveTo(118,48);
+ctx3.lineTo(74,48);
+ctx3.stroke();
+ctx3.closePath();
 
+var c4=document.getElementById('canvas4');
+var ctx4=c4.getContext('2d');
+ctx4.fillStyle="Red";
+ctx4.strokeStyle="Orange";
+//ctx4.lineWidth=5;
+ctx4.beginPath();
+ctx4.arc(50,50,25,0,Math.PI*2);
+ctx4.arc(50,50,25,0,Math.PI*2);
+ctx4.stroke();
+ctx4.fill();
+ctx4.closePath();
 
-function start(){
-	
-		ballMove(myBallColor);
-		let leaderText="highscore";
-if(localStorage[leaderText])
-		displayLeaderboards();
-}
+var c5=document.getElementById('canvas5');
+var ctx5 = c5.getContext('2d');
 
-function end(){
-	displayLeaderboards();
-	y=canvas.height*0.85;
-	obsY=-outerRadius*2;
-	obsVelocity=2;
-	offSet=0;
-	offSetInc=0.02;
-	diffInc=0;
-	velocity=0;
-	
-	
-}
-//let circle=new ballMove(myBallColor);
-function ballMove(myBallColor){
-	
-	//requestAnimationFrame(colorChangers);
-	
-	requestAnimationFrame(circleObstacles);
-	
-	requestAnimationFrame(ballMove);
-	
-	
-this.color=myBallColor;
-c.beginPath();
-c.arc(canvas.width/2, y, 15, 0, Math.PI*2);
-c.fillStyle=this.color;
-c.strokeStyle="#fff";
-c.save();
-
-//console.log(myBallColor);
-c.fill();
-c.stroke();
-var newColor;
-/*this.colorChange=function(newColor)
-{
-	this.color=newColor;
-	myBallChange=newColor;
-};*/
-if(velocity!=0){
-y+=velocity;
-velocity+=acc;
-if(y>=canvas.height*0.85)
-	velocity=0;
-}
-}
-		
-window.addEventListener('click', function(){
-	mySound.play();
-	velocity=-4.2;
-	
-});
-
-
-
-//large obstacles
-	var outerRadius=150;
-	var innerRadius=125;
-var obsY=-outerRadius*2;
-var obsVelocity=2;
-var offSet=0;
-var offSetInc=0.02;
-var diffInc=0;
-function circleObstacles(){
-	c.clearRect(0,0, innerWidth, innerHeight);
-	//c.clearRect(canvas.width/2-outerRadius, obsY-outerRadius, outerRadius*2, outerRadius*2);
-	let bottomColor;
-	let topColor;
-	if(diffInc>500)
-	{
-		obsVelocity++;
-		offSetInc+=0.01;
-		diffInc=0;
-	}
-	//if(!paused){
-	for(let i=0;i<4;i++)
-	{
-		var startAngle=((Math.PI*2)/4)*i+offSet;
-		var endAngle=((Math.PI*2)/4)*(i+1)+offSet;
-		c.beginPath();
-		c.arc(canvas.width/2, obsY, outerRadius, startAngle, endAngle);
-		c.fillStyle=colorArray[i];
-		c.lineTo(canvas.width/2, obsY);
-		c.fill();
-		if(startAngle%(Math.PI*2)>=0 && startAngle%(Math.PI*2)<=Math.PI/2){
-			bottomColor=colorArray[i];
-		}
-		else if(startAngle%(Math.PI*2)>Math.PI && startAngle%(Math.PI*2)<1.5*Math.PI){
-			topColor=colorArray[i];
-		}
-	}
-	//console.log(bottomColor);
-	//console.log(topColor);
-	c.beginPath();
-	c.arc(canvas.width/2, obsY, innerRadius, 0, Math.PI*2);
-	c.fillStyle="#000";
-	c.fill();
-	c.restore();
-	if(obsY-(outerRadius*4.5)>canvas.height)
-		obsY=-outerRadius*2;
-	if(!paused){
-	obsY+=obsVelocity;
-	offSet+=offSetInc;
-	score++;
-	console.log(score);
-	diffInc++;
-	//console.log('diffInc'+diffInc);
-	}
-	//if(offSet==2)
-		//offSet=0;
-	if(y-15<obsY+outerRadius && y+15>obsY+innerRadius)
-	{
-		if(bottomColor!=myBallColor)
-		{
-			alert(score);
-			update(score);
-			end();
-			location.reload();
-		}
-			
-	}
-	
-	if(y-15<obsY-innerRadius && y+15>obsY-outerRadius)
-	{
-		if(topColor!=myBallColor)
-		{
-			alert(score);
-			update(score);
-			end();
-		}
-	}
-	
-}
-	
-	
-//color changing
-
-	var smallRadius=15;
-var obsSmallY=-smallRadius*10-obsY;
-var obsSmallVelocity=2;
-var offSmallSet=0;
-/*
-function colorChangers(){
-	
-	
-	//c.clearRect(canvas.width/2-outerRadius, obsY-outerRadius, outerRadius*2, outerRadius*2);
-	let bottomColorSmall;
-	//if(!paused){
-	for(let i=0;i<4;i++)
-	{
-		var startAngle=((Math.PI*2)/4)*i+offSmallSet;
-		var endAngle=((Math.PI*2)/4)*(i+1)+offSmallSet;
-		c.beginPath();
-		c.arc(canvas.width/2, obsSmallY, smallRadius, startAngle, endAngle);
-		c.fillStyle=colorArray[i];
-		c.lineTo(canvas.width/2, obsSmallY);
-		c.fill();
-		if(startAngle%(Math.PI*2)>=0 && startAngle%(Math.PI*2)<=Math.PI/2){
-			bottomColorSmall=colorArray[i];
-		}
-	}
-	//console.log(bottomColorSmall);
-	
-	if(y-15<obsSmallY+smallRadius && y-15>obsSmallY-smallRadius)
-	{
-		
-		c.fillStyle=bottomColorSmall;
-		c.restore();
-		c.save();
-	}
-	//}
-	c.save();
-	c.restore();
-	if(obsSmallY-(smallRadius*8)>canvas.height)
-		obsSmallY=-smallRadius*2;
-	if(!paused){
-	obsSmallY+=obsSmallVelocity;
-	offSmallSet+=0.02;
-	}
-	//if(offSet==2)
-		//offSet=0;
-	
-	
-}	
-
-*/
-function update(score)
-{
-	
-	var highscore=localStorage.getItem("highscore");
-			if(highscore!=null)
-			{
-				if(score>highscore)
-				{
-					localStorage.setItem("highscore", score);
-					alert('new highscore');
-					location.reload();
-				}
-				location.reload();
-			}
-			else
-				{
-					localStorage.setItem("highscore", score);
-					alert('new highscore');
-				};
-			
-			
-	
-	
 }
